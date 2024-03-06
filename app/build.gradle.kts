fdec14916342839a122338bfd73a29b05bf7029b
valeriafern <vfernandez321@misena.edu.co>
2024-03-06 13:19:04 -0500

    corregido

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,10 +61,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-<<<<<<< HEAD
     implementation("com.google.android.material:material:1.2.1")
-=======
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.picasso:picasso:2.71828")
->>>>>>> 5c9189871b757fbb712a8d7a4e66fbe2991e269b
 }
